a132a56d0e1be90b070491e696f90a4aa95fa6a6
Rosen Stoyanov <rosen.stoyanov@stanga.net>
2018-01-18 16:00:23 +0200

    init create new Ride

---
+++ SharedTogether/SharedTogether/RideTableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RideTableTableViewCell.swift
+//  SharedTogether
+//
+//  Created by Rosen Stoyanov on 16.01.18.
+//  Copyright © 2018 SharedTogether Team. All rights reserved.
+//
+
+import UIKit
+
+class RideTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
